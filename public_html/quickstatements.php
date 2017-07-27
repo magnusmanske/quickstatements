3c6bc62afaea4f683acbc9568dcf3968d412a248
Magnus Manske <magnusmanske@gmail.com>
2017-07-27 10:19:16 +0000

    Manually applying patch D723 by matej_suchanek

--- public_html/quickstatements.php
+++ public_html/quickstatements.php
@@ -880,21 +880,21 @@ if ( !isset($o->id) ) print_r ( $o ) ;
 			return true ;
 		}
 		
-		if ( preg_match ( '/^[\+\-]{0,1}\d+(\.\d+){0,1}$/' , $v ) ) { // Quantity
+		if ( preg_match ( '/^([\+\-]{0,1}\d+(\.\d+){0,1})(U(\d+)){0,1}$/' , $v , $m ) ) { // Quantity
 			$cmd['datavalue'] = array ( "type"=>"quantity" , "value"=>array(
-				"amount" => $v*1 ,
-				"unit" => "1"
+				"amount" => $m[1]*1 ,
+				"unit" => isset( $m[4] ) ? "http://www.wikidata.org/entity/Q{$m[4]}" : "1"
 			) ) ;
 			return true ;
 		}
-		if ( preg_match ( '/^([\+\-]{0,1}\d+(\.\d+){0,1})\s*~\s*([\+\-]{0,1}\d+(\.\d+){0,1})$/' , $v , $m ) ) { // Quantity with error
+		if ( preg_match ( '/^([\+\-]{0,1}\d+(\.\d+){0,1})\s*~\s*([\+\-]{0,1}\d+(\.\d+){0,1})(U(\d+)){0,1}$/' , $v , $m ) ) { // Quantity with error
 			$value = $m[1]*1 ;
 			$error = $m[3]*1 ;
 			$cmd['datavalue'] = array ( "type"=>"quantity" , "value"=>array(
 				"amount" => $value ,
 				"upperBound" => $value+$error ,
 				"lowerBound" => $value-$error ,
-				"unit" => "1"
+				"unit" => isset( $m[6] ) ? "http://www.wikidata.org/entity/Q{$m[6]}" : "1"
 			) ) ;
 			return true ;
 		}
