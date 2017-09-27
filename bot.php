8edf39a8acf0a4cfce4d13da1cff2e625d480fd9
Magnus Manske <magnusmanske@gmail.com>
2017-09-27 12:20:34 +0000

    misc

--- bot.php
+++ bot.php
@@ -1,20 +1,15 @@
 #!/usr/bin/php
 <?PHP
 
-#error_reporting(E_ERROR|E_CORE_ERROR|E_ALL|E_COMPILE_ERROR);
-
 require_once ( '/data/project/quickstatements/public_html/quickstatements.php' ) ;
 
 function iterate() {
 	$ret = 0 ;
 	$qs = new QuickStatements ;
 	$db = $qs->getDB() ;
-	$pipes = array() ;
 	$sql = "SELECT id,status FROM batch WHERE status IN ('INIT','RUN')" ;
 	if(!$result = $db->query($sql)) die('There was an error running the query [' . $db->error . ']');
 	while($o = $result->fetch_object()){
-//		$pipes[] = popen ( "/data/project/quickstatements/run_single_command.php {$o->id}" , 'r' ) ;
-
 		$qs2 = new QuickStatements ;
 		if ( $o->status == 'INIT' ) {
 			if ( !$qs2->startBatch ( $o->id ) ) {
@@ -24,20 +19,14 @@ function iterate() {
 		}
 		if ( !$qs2->runNextCommandInBatch ( $o->id ) ) print $qs2->last_error_message."\n" ;
 		else $ret++ ;
-
 	}
-/*
-	foreach ( $pipes AS $pipe ) {
-		$ret++ ;
-		pclose ( $pipe ) ;
-	}
-*/
 	return $ret ;
 }
 
 while ( 1 ) {
 	$worked = iterate() ;
 	if ( $worked == 0 ) sleep ( 5 ) ;
+	else sleep ( 0.5 ) ;
 }
 
 ?>
---
+++ new_job_from_qs.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+<?PHP
+
+#error_reporting(E_ERROR|E_CORE_ERROR|E_ALL|E_COMPILE_ERROR);
+$user_id_magnus = 4420 ;
+
+require_once ( '/data/project/quickstatements/public_html/quickstatements.php' ) ;
+
+if ( !isset($argv[1]) ) die ( "Requires QS file as parameter\n" ) ;
+$fn = $argv[1] ;
+if ( !file_exists($fn) ) die ( "File $fn does not exist\n" ) ;
+$commands = file_get_contents ( $fn ) ;
+
+$batch_name = isset($argv[2]) ? $argv[2] : '' ;
+
+$qs = new QuickStatements ;
+$j = $qs->importData ( $commands , 'V1' , true ) ;
+$batch_id = $qs->addBatch ( $j['data']['commands'] , $user_id_magnus , $batch_name ) ;
+print "Now as batch #$batch_id with " . count($j['data']['commands']) . " commands\n" ;
+
+?>
