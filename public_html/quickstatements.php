magnusmanske/quickstatements

commit 8d516dea588e0b0ada237504ba927860f23ae440

Resolves #34

public_html/quickstatements.php
@@ -548,27 +548,30 @@ class QuickStatements {
 	protected function getStatementID ( $command ) {
 		if ( !$this->isProperty ( $command->property ) ) return ;
 		if ( !isset($command->datavalue) ) return ;
+		if ( isset($command->new_statement) and $command->new_statement ) return ;
 		$q = $command->item ;
 
 		$this->wd->loadItem ( $q ) ;
 		if ( !$this->wd->hasItem($q) ) return ;
 		$i = $this->wd->getItem ( $q ) ;
 		$claims = $i->getClaims ( $command->property ) ;
+		$last_claim_id = null ;
 		foreach ( $claims AS $c ) {
 			// when snaktype is somevalue/novalue, $c->mainsnak->datavalue doesn't exist (since the value is unknown or not existing)
 			if ( $c->mainsnak->snaktype === "somevalue" || $c->mainsnak->snaktype === "novalue" ) {
 
 				$lackingValueType = $c->mainsnak->snaktype ;
-				if ( $lackingValueType === $command->datavalue->type ) return $c->id ;
+				if ( $lackingValueType === $command->datavalue->type ) $last_claim_id = $c->id ; // return $c->id ;
 
 			} else {
 
 				if ( !isset($c->mainsnak) or !isset($c->mainsnak->datavalue) ) continue ;
 				if ( !isset($command->datavalue) ) continue ;
-				if ( $this->compareDatavalue ( $c->mainsnak->datavalue , $command->datavalue ) ) return $c->id ;
+				if ( $this->compareDatavalue ( $c->mainsnak->datavalue , $command->datavalue ) ) $last_claim_id = $c->id ; // return $c->id ;
 
 			}
 		}
+		return $last_claim_id ;
 	}
 	
 	// Return true if both datavalues are the same (for any given value of same...), or false otherwise
@@ -632,7 +635,8 @@ class QuickStatements {
 						'mainsnak' => array (
 							'snaktype' => 'value' ,
 							'property' => $commands[$pos]['property'] ,
-							'datavalue' => $commands[$pos]['datavalue']
+							'datavalue' => $commands[$pos]['datavalue'],
+							'new_statement' => $commands[$pos]['new_statement']
 						) ,
 						'type' => 'statement' ,
 						'rank' => 'normal'
@@ -1227,9 +1231,14 @@ exit ( 1 ) ; // Force bot restart
 				$cols[0] = $m[1] ;
 			}
 			$first = strtoupper(trim($cols[0])) ;
-			if ( count ( $cols ) >= 3 and ( $this->isValidItemIdentifier($first) or $first == 'LAST' ) and $this->isValidItemIdentifier($cols[1]) ) {
-				$prop = strtoupper(trim($cols[1])) ;
-				$cmd = array ( 'action'=>$action , 'item'=>$first , 'property'=>$prop , 'what'=>'statement' ) ;
+			if ( count ( $cols ) >= 3 and ( $this->isValidItemIdentifier($first) or $first == 'LAST' ) and ( $this->isValidItemIdentifier($cols[1]) or preg_match ( '/^(!P)(\d+)$/i' , $cols[1] ) ) ) {
+        			$prop = strtoupper(trim($cols[1])) ;
+	        		$is_new_statement = 0 ;
+        			if ( $prop[0] == '!') {
+	        		        $is_new_statement = 1 ;
+		        	        $prop = substr($prop, 1) ;
+        			}
+				$cmd = array ( 'action'=>$action , 'item'=>$first , 'property'=>$prop , 'what'=>'statement', 'new_statement'=>$is_new_statement ) ;
 				if ( $comment != '' ) $cmd['summary'] = $comment ;
 				$this->parseValueV1 ( $cols[2] , $cmd ) ;
 

commit 0d9ae3b07cca758eb6aed697f3d098a76a41e002

Fix statement creation in CSV mode
This was broken by 8d516dea58. The new $statement field 'new_statement'
is checked via isset() in getStatementID(), but unconditionally assumed
to exist in compressCommands(). However, only importDataFromV1() was
taught to create this field, while importDataFromCSV() wasn’t. This
meant that parsing CSV statements would produce a PHP warning, which is
included in the web response and thus breaks the JSON output.

Fixes #38.

An alternative would be to guard the access in compressCommands(); the
easiest option would be the `??` operator, but that was only added in
PHP 7.0 and I don’t really want to risk breaking sites that use
QuickStatements on older PHP (even though PHP 7.0 has been out for a
while already).

public_html/quickstatements.php
@@ -1382,6 +1382,7 @@ exit ( 1 ) ; // Force bot restart
                 if ( $instruction[0] === 'P' ) {
                     $command += [
                         'what' => 'statement',
+                        'new_statement' => 0,
                         'property' => $instruction
                     ];
                     $this->parseValueV1( $value, $command );